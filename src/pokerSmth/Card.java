edf762d1493401ff02a80f4e7bb9db4ca6311a02
aaalliii <jj24ers@gmail.com>
2024-08-18 19:17:50 +0500

    seventh commit (i delete unused code in Card.java)

--- src/pokerSmth/Card.java
+++ src/pokerSmth/Card.java
@@ -1,7 +1,5 @@
 package pokerSmth;
 
-import java.util.Objects;
-
 public class Card {
     public String cardName;
     public Suit cardSuit;
@@ -9,19 +7,6 @@ public class Card {
     public Card(String cardName, Suit cardSuit) {
         this.cardName = cardName;
         this.cardSuit = cardSuit;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Card card = (Card) o;
-        return cardName.equals(card.cardName) && cardSuit == card.cardSuit;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(cardName, cardSuit);
     }
 
     @Override
